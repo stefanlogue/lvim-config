776d5ab9964cd050515ebd3761b00242025c5cba
stefanlogue <stefanlogue@icloud.com>
2023-05-24 10:06:39 +0100

    make Neotree toggle instead of focus

--- config.lua
+++ config.lua
@@ -41,7 +41,7 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-lvim.keys.normal_mode["<S-e>"] = ":Neotree<cr>"
+lvim.keys.normal_mode["<S-e>"] = ":Neotree toggle<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
